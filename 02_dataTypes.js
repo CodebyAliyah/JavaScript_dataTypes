5c52392c1c006f02aaacbc25dfe96ae719838f87
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-07 11:06:48 -0700

    Assignment Successfully

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"  // treat all js code in newer version.
+
+alert ("Hello World")  // we are using nodejs, not browser.
+ 
+console.log (3 + 3)  // code readabilty should be high.
+
+let userName = "Ali yahya" ;
+
+let age = 21 
+
+let youeAge = true
+
+ /* DataTypes  */
+
+// number => 2 to power 53 
+// bigint 
+// string => ""
+// boolean => false/true
+// Null => standalone value
+// symbol => umique
+
+console.log(typeof null); // object ;
+
+console.log (typeof undefined) // undefined ;
